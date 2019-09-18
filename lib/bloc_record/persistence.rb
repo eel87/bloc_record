797ce0b5e0c6f17f82ccdf6bbce5958b87764c41
erinleck@gmail.com <erinleck@gmail.com>
2019-09-18 10:01:27 -0600

    checkpoint 5 destroy checkpoint work

--- lib/bloc_record/persistence.rb
+++ lib/bloc_record/persistence.rb
@@ -36,6 +36,10 @@ module Persistence
     self.class.update(self.id, updates)
   end
 
+  def destroy
+    self.class.destroy(self.id)
+  end
+
   module ClassMethods
     def update_all(updates)
       update(nil, updates)
@@ -76,5 +80,38 @@ module Persistence
 
       true
     end
+
+    def destroy(*id)
+      if id.length > 1
+        where_clause = "WHERE id IN (#{id.join(",")});"
+      else
+        where_clause = "WHERE id IN #{id.first};"
+      end
+
+      connection.execute <<-SQL
+        DELETE FROM #{table} #{where_clause}
+      SQL
+
+      true
+    end
+
+    def destroy_all(conditions_hash=nil)
+      if conditions_hash && !conditions_hash.empty?
+        conditions_hash = BlocRecord::Utility.convert_keys(conditions_hash)
+        conditions = conditions_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+      
+        connection.execute <<-SQL
+          DELETE FROM #{table}
+          WHERE #{conditions};
+        SQL
+      else
+        connection.execute <<-SQL
+          DELETE FROM #{table};
+        SQL
+      end
+      
+      true
+    end
+
   end
 end
